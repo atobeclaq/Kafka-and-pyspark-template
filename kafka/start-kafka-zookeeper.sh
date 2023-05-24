feef5c02861936a8a53ed016202b54afab177105
michen <atobeclaq@gmail.com>
2023-05-24 23:42:33 +1000

    feat: permission adds and automation basic

--- kafka/start-kafka-zookeeper.sh
+++ kafka/start-kafka-zookeeper.sh
@@ -5,17 +5,29 @@ current_dir=$(cd "$(dirname "$0")" && pwd)
 
 # Function to check if ZooKeeper has started
 check_zookeeper_started() {
-    nc -z localhost 2181
-    return $?
+    nc -z localhost 2181 < /dev/null
+    lsof -i :2181
+    echo $(nc -z localhost 2181 < /dev/null)
+    echo $(lsof -i :2181)
+
+    while true; do
+        lsof -i :2181 | grep LISTEN > /dev/null
+        if [ $? -eq 0 ]; then
+            echo "ZooKeeper is running on port 2181."
+            break
+        else
+            echo "ZooKeeper is not running on port 2181. Retrying..."
+            sleep 1
+        fi
+    done
 }
+
 
 # Start ZooKeeper in a new terminal
 osascript -e "tell application \"Terminal\" to do script \"cd '$current_dir' && ./start-zookeeper.sh\""
 
 # Wait for ZooKeeper to start
-while ! check_zookeeper_started; do
-    sleep 1
-done
+check_zookeeper_started
 
 # Start Kafka in another terminal
 osascript -e "tell application \"Terminal\" to do script \"cd '$current_dir' && ./start-kafka.sh\""

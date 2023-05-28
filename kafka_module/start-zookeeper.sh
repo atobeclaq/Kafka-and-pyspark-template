41ea645aa3cfbd0b57f9adc78c014a0cdb1bc94a
michen <atobeclaq@gmail.com>
2023-05-28 20:41:32 +1000

    fix: fix the typo

--- kafka_module/start-zookeeper.sh
+++ kafka_module/start-zookeeper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Start ZooKeeper
-cd /Path/to/kafka_2.12-3.4.0/kafka_2.12-3.4.0
+cd /Path/to/kafka_2.12-3.4.0
 # properties can redirect to the server.properties in this directory
 bin/zookeeper-server-start.sh config/zookeeper.properties
